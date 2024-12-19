e8a02c3566300140ee7c126065af1b84b5175a48
MxMElmBs <eagbezou@hotmail.com>
2024-12-19 14:06:17 +0000

    Update image for PWA

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-self.addEventListener("install", e =>{
+self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./css/style.css", "./images/OIG (1).jpg"]);
-        })
+      caches.open("static").then(cache => {
+        return cache.addAll(["./", "./css/style.css", "./images/images/OIG (1) (2).jpg",
+            "./images/images/OIG (1) (1).jpg", "./images/images/OIG (1).jpg"
+        ]); // Mettre à jour les chemins des fichiers
+      })
     );
-});
-
-self.addEventListener("fetch", e=> {
+  });
+  
+  self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request);
+      })
     );
-});
-
+  });
+  
